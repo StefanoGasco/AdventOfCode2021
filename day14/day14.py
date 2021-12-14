81a933ab9ca63aee166ee75027d080b1330f0842
gasco <stefano.gasco@copebit.ch>
2021-12-14 16:39:30 +0100

    day14 part2

--- day14/day14.py
+++ day14/day14.py
@@ -11,17 +11,32 @@ def input_file():
 
 
 pattern, mapping = input_file()
+res = {}
+shortcut = {}
+
+def sum_dict(d1, d2):
+    return {k: d1.get(k, 0) + d2.get(k, 0) for k in set(d1) | set(d2)}
+
+lookup = dict()
 
 def rebuild(couple, threshold=1):
     if threshold <= 0:
-        return couple
-    couple.insert(1,mapping["".join(couple)])
-    return rebuild(couple[:2],threshold-1) + rebuild(couple[1:3],threshold-1)[1:]
+        return dict()
+    couple_str = "".join(couple)
+    if lookup.get(couple_str, {}).get(threshold, None):
+        return lookup[couple_str][threshold]
+    new = mapping[couple_str]
+    res = {new:1}
+    res = sum_dict(res, rebuild([couple[0], new],threshold-1))
+    res = sum_dict(res, rebuild([new, couple[1]],threshold-1))
+    lookup.setdefault(couple_str, {}).setdefault(threshold, {})
+    lookup[couple_str][threshold] = res
+    return res
 
-res = {pattern[0]: 1}
-
+final = dict()
+for x in pattern:
+    final[x] = final.get(x, 0) + 1
 for x in range(len(pattern)-1):
-    for k in rebuild(pattern[x:x+2], threshold=10)[1:]:
-        res[k] = res.get(k, 0) + 1
+    final = sum_dict(final, rebuild(pattern[x:x+2], threshold=40))
 
-print(max(res.values())-min(res.values()))
+print(max(final.values())-min(final.values()))
